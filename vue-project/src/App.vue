f3319e7ce5d405726eb7817bb83a0c893a927795
Alex <alexsword561@gmail.com>
2023-05-17 14:35:57 +0300

    Update App.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="switch">
-
-
         <div class="demo">
             <div class="demo__left">
                 <my-multiselect
-                        :options="values"
-                        display-property="title"
-                        value-property="shortcut"
-                        v-model="selectedValue"
+                    :options="values"
+                    display-property="title"
+                    value-property="shortcut"
+                    v-model="selectedValue"
+
                 ></my-multiselect>
             </div>
         </div>
@@ -22,21 +21,19 @@ export default {
     data() {
         return {
             values: [
-                { title: "foo", shortcut: "value1" },
-                { title: "bar", shortcut: "value2" },
-                { title: "fizz", shortcut: "value3" },
-                { title: "buzz", shortcut: "value4" },
-                { title: "foo2", shortcut: "value5" },
+                { title: "bar", shortcut: "value1" },
+                { title: "boo", shortcut: "value2" },
+                { title: "far", shortcut: "value3" },
+                { title: "root", shortcut: "value4" },
+                { title: "else", shortcut: "value5" },
                 { title: "bar2", shortcut: "value6" },
-                { title: "fizz2", shortcut: "value7" }
+                { title: "fizz2", shortcut: "value7" },
             ],
             selectedValue: ["value1", "value2", "value3", "value4"]
         };
     },
     components: {
-        MyMultiselect
+        MyMultiselect,
     },
-
-    methods: {}
 };
 </script>
